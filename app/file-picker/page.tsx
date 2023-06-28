300044f0f513e661cfce135874db416e6b23ad49
owolfdev <owolfdev@gmail.com>
2023-06-29 06:37:16 +0700

    update file picker

--- app/file-picker/page.tsx
+++ app/file-picker/page.tsx
@@ -6,7 +6,7 @@ const FilePickerPage = () => {
     <section className="container grid items-center gap-6 pt-6 pb-8 md:py-10">
       <div className="flex max-w-[980px] flex-col items-start gap-2">
         <h1 className="text-3xl font-extrabold leading-tight tracking-tighter md:text-4xl">
-          Custom form demo.
+          Custom file picker.
         </h1>
       </div>
       <div className="flex gap-2">
